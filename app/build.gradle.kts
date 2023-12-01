9ab55ca5f74077e458eaba34046b4ace3e2f1538
Aeoline <raubrey.kh@gmail.com>
2023-12-02 03:21:37 +0700

    navbar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,5 @@ dependencies {
 
     implementation("com.github.denzcoskun:ImageSlideShow:0.1.2")
     implementation ("de.hdodenhof:circleimageview:3.1.0")
+    implementation ("com.google.android.material:material:1.12.0-alpha01")
 }
